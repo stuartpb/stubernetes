b02a2fb3056820f1e86a83910af5929003a45f79
Stuart P. Bentley <s@stuartpb.com>
2020-10-03 03:53:41 -0700

    Add options to load root keys in Combustion

--- config-imagers/combustion/mk-combustion-script.sh
+++ config-imagers/combustion/mk-combustion-script.sh
@@ -12,10 +12,16 @@ if [[ -n $SET_HOSTNAME ]]; then
   echo "hostnamectl set-hostname $SET_HOSTNAME"
 fi
 
+if [[ -n $HASHED_ROOT_PASSWORD ]]; then
+  echo "echo 'root:$HASHED_ROOT_PASSWORD' | chpasswd -e"
+elif [[ -n $UNHASHED_ROOT_PASSWORD ]]; then
+  echo "echo 'root:$(openssl passwd -6 "$UNHASHED_ROOT_PASSWORD")' | chpasswd -e"
+fi
+
 if [[ -n $ROOT_KEY_SOURCE ]]; then
   echo "mkdir -p /root/.ssh
-  curl $ROOT_KEY_SOURCE > /root/.ssh/authorized_keys
-  chmod 0600 /root/.ssh/authorized_keys"
+curl $ROOT_KEY_SOURCE > /root/.ssh/authorized_keys
+chmod 0600 /root/.ssh/authorized_keys"
 fi
 
 cat steps/disable-lid-switch.sh steps/kubic/setup-systemd-networkd.sh
