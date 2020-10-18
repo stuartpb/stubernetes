41555f8108829e3d46b93ef61d3abdf7cc84e7f7
Stuart P. Bentley <s@stuartpb.com>
2020-10-17 19:47:25 -0700

    Init cluster with kube-router instead of Calico

--- hack/cluster-setup/init-cluster.sh
+++ hack/cluster-setup/init-cluster.sh
@@ -1,2 +1,2 @@
 kubeadm init --image-repository k8s.gcr.io --pod-network-cidr=10.244.0.0/16
-KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
