0baa482d0e6e74ec5bce9528f279a10fe665ea1f
Stuart P. Bentley <s@stuartpb.com>
2020-10-17 21:20:04 -0700

    Init cluster with Flannel
    I ended up having issues with kube-router, too, and if I'm going to
    experiment further, Flannel is the only remaining option supported by the
    upstream Kubic team.

--- hack/cluster-setup/init-cluster.sh
+++ hack/cluster-setup/init-cluster.sh
@@ -1,2 +1,2 @@
 kubeadm init --image-repository k8s.gcr.io --pod-network-cidr=10.244.0.0/16
-KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
